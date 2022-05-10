ffd7d402c21c10a77cc8ad211ad854227ccf7ace
Olubusayo Amowe <amoweolubusayo@gmail.com>
2022-05-10 20:30:41 +0100

    changed a href to Link href

--- components/Navbar.js
+++ components/Navbar.js
@@ -86,12 +86,11 @@ export default function Navbar() {
                   Visit Dashboard
                 </a> */
                 }
-                <a
-                  href="/dashboard"
-                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Visit Dashboard
-                </a>
+                <Link href="/dashboard">
+                  <a className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-full text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Visit Dashboard
+                  </a>
+                </Link>
               </span>
             ) : (
               <span>Not connected</span>
