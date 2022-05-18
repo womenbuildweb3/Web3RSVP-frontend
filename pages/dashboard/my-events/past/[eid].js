66d00451ebdc47285049f31146a78409b61acc21
Sarah Zhang <15064710+sazhang@users.noreply.github.com>
2022-05-17 22:19:29 -0700

    fix placeholder

--- pages/dashboard/my-events/past/[eid].js
+++ pages/dashboard/my-events/past/[eid].js
@@ -1,5 +1,10 @@
 import Dashboard from "../../../../components/Dashboard";
 
+const navigation = [
+  { name: "My Events", href: "/dashboard/my-events", current: true },
+  { name: "My RSVPs", href: "/dashboard/my-rsvps", current: false },
+];
+
 const tabs = [
   { name: "Upcoming", href: "/dashboard/my-events/upcoming", current: false },
   { name: "Past", href: "/dashboard/my-events/past", current: true },
@@ -7,7 +12,7 @@ const tabs = [
 
 const UpdateMyPastEvent = () => {
   return (
-    <Dashboard title={"My Events"} tabs={tabs}>
+    <Dashboard navigation={navigation} title={"My Events"} tabs={tabs}>
       <p>coming soon</p>
     </Dashboard>
   );
